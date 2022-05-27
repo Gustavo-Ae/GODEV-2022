9ee734dc89022ed8df57c528132d515182005e68
Gustavo-Ae <gustavoaedias05@hotmail.com>
2022-05-27 10:32:15 -0300

    Strings em JavaScript

---
+++ Aulas_Gravadas/HTML_CSS_JS/aula 03 - JavaScript/main.js
@@ -0,0 +1,18 @@
+//Strings 
+
+const firstName = "Gustavo"
+const lastName = "Araújo"
+
+const names = "Felipe, Ana , João"
+
+console.log("Meu nome é "+firstName +" "+lastName) // quantos caracteres a string tem
+console.log(`Meu nome é ${firstName} ${lastName}`) //pode fazer da mesma maneira utilizando aspinhas.
+console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}`) // .toUpperCase() = deixa tudo em Maiúsculo. toLowerCase() = deixa tudo em Minúsculo.
+
+console.log(names.split(",")) 
+
+const number = 5
+
+console.log(number.toString()) //Converte o número para String 
+
+
