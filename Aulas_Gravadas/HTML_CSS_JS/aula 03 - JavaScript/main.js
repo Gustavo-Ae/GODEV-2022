ffa91066c6d1dcdfafa730f4a064d0425c8b2f6e
Gustavo-Ae <gustavoaedias05@hotmail.com>
2022-05-27 23:37:16 -0300

    Funções e ArrowFunctions - JS

--- Aulas_Gravadas/HTML_CSS_JS/aula 03 - JavaScript/main.js
+++ Aulas_Gravadas/HTML_CSS_JS/aula 03 - JavaScript/main.js
@@ -1,9 +1,20 @@
-// Truthy e Falsy 
+// Funções
 
-// !! - verifica se é verdeiro ou falso
+function sum(a,b){
+    return a + b
+}
 
-const x = ""
+const sumValue = sum(2,2)
 
-console.log(!!x)
+console.log(sumValue)
 
+// ArrowFunctions - Uma forma diferente de escrever funções:
+
+const sumArrow1 = (a,b) => {
+    return a + b
+}
+
+const sumArrow = (a,b) => a + b   //dessa forma é bem mais rápido e simples, e faz a mesma coisa que a função de cima
+
+console.log(sumArrow(10,10))
 
