f34edb75c35bcd390154498fb94ef190fa121e32
Gustavo-Ae <gustavoaedias05@gmail.com>
2022-06-01 20:56:32 -0300

    Objeto - JS

--- Aulas_Gravadas/HTML_CSS_JS/aula 03 - JavaScript/main.js
+++ Aulas_Gravadas/HTML_CSS_JS/aula 03 - JavaScript/main.js
@@ -1,20 +1,56 @@
-// Funções
+// Objeto
 
-function sum(a,b){
-    return a + b
+class Person{
+    constructor(firstName, LastName, age) {
+        this.firstName = firstName
+        this.LastName = LastName
+        this.age = age
+    }
+    getFullName(){ //Isso é um método
+        console.log(`${this.firstName} ${this.LastName}`)
+    }
+    //Podemos criar metodos estaticos(static) para não precisar instanciar a classe , vai ser utilizado quando não precisar utilizar as caracteristicas do objetos :
+
+    static speak(){
+        console.log("Hello World!")
+    }
+
 }
 
-const sumValue = sum(2,2)
+const person = new Person("Jane", "Doe", 40) //Aqui estamos instanciando(criando) uma classe e isso é um Objeto.
 
-console.log(sumValue)
+console.log(person)
 
-// ArrowFunctions - Uma forma diferente de escrever funções:
+console.log(person.getFullName())
 
-const sumArrow1 = (a,b) => {
-    return a + b
+Person.speak()  // Utiliza assim para chamar métodos estáticos(static)
+
+//Herança - Objeto
+
+class Animal{
+    constructor(name) {
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} made some noise!`)
+    }
 }
 
-const sumArrow = (a,b) => a + b   //dessa forma é bem mais rápido e simples, e faz a mesma coisa que a função de cima
+class Dog extends Animal{
+    constructor(name){
+        super(name)   //Chama o construtor da classe pai
+    }
 
-console.log(sumArrow(10,10))
+    speak(){
+        console.log(`${this.name} barked!`)
+    }
+}
+
+// const animal = new Animal("Simba")
+
+const dog = new Dog("Bob")
+
+dog.speak()
+
+
 
