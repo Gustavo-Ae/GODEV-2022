ed63955c0b65c06473a7e61bb97848b2a13f3acd
Gustavo-Ae <gustavoaedias05@hotmail.com>
2022-06-16 12:52:35 -0300

    Promises com Async/Await - JS

--- Aulas_Gravadas/HTML_CSS_JS/aula 04 - JavaScript Assincrono/index.js
+++ Aulas_Gravadas/HTML_CSS_JS/aula 04 - JavaScript Assincrono/index.js
@@ -1,22 +1,51 @@
 
-// Promise.all - Utilizado quando quiser executar tudo ao mesmo tempo. Lembre-se : Ele vai esperar até a última promise seja resolvida para mostrar o resultado na tela, que nesse caso é o facebook com tempo de 3s.
+// --------------------- Utilizando Promises com Async/Await  : 
 
-const videoYouTube = new Promise(resolve => {  // quando a função não recebe nenhum parametro, você pode fazer dessa forma :
+//
 
-    setTimeout(() => {
-        resolve("videos from YouTube")
-    }, 2000)
-})
+const loginUser = (email, password) => {
+    return new Promise((resolve, reject) => {
+        const error = false
 
-const facebook = new Promise((resolve) => {
+        if(error){
+            reject(new Error("error in login!"))
+        }
+
+        console.log("user logged!")
+        resolve({email})
+    })
+}
+
+const getUserVideos = (email) => {
     
-    setTimeout(() => {
-        resolve("posts from facebook")
-    }, 3000) 
-})
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("getUserVideos")
+            resolve(["video1", "video2", "video3"])
+        }, 2000)
+    })
+} 
 
-Promise.all([videoYouTube, facebook]).then(result => {
-    console.log({result})
-})
+const getVideoDetails = (video) => {
 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("getVideoDetails")
+            resolve({title:"video title"})
+        },2500)
+    })
 
+}
+
+const displayUser = async() => {
+    try{
+        const user = await loginUser("fernando@gmail.com", "123456") //Await - vai esperar a promise loginUser() ser resolvida para ae sim, ele ir para a próxima linha.
+        const videos = await getUserVideos(user.email)
+        const videosDetails = await getVideoDetails(videos[0])
+        console.log({videosDetails})
+    }catch(error){
+        console.log({error})
+    }
+}
+
+displayUser()
