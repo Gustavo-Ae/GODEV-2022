4221568ef5be772dd2255e6c354d9c51bb658406
Gustavo-Ae <gustavoaedias05@hotmail.com>
2022-06-21 12:07:01 -0300

    Conectando com o banco de dados e utilizando o método POST e GET

--- Aulas_Gravadas/Node/index.js
+++ Aulas_Gravadas/Node/index.js
@@ -1,8 +1,19 @@
-const { Person } = require("./person")
+// const { Person } = require("./person")
 
-const person = new Person("Gustavo")
+// const person = new Person("Gustavo")
 
-//require("./modules/fs")
+// //require("./modules/fs")
+
+
+const dotenv = require("dotenv")
+const connectToDatabase = require("./src/database/connect")
+
+dotenv.config()
+
+connectToDatabase()
 
 require("./modules/express")
+
+
+
 
--- Aulas_Gravadas/Node/modules/express.js
+++ Aulas_Gravadas/Node/modules/express.js
@@ -1,6 +1,10 @@
 const express = require("express")
 
+const UserModel = require("../src/models/user.model")
+
 const app = express()
+
+app.use(express.json())
 
 const port = 8080
 
@@ -9,19 +13,26 @@ app.get("/home", (req, res) => {
     res.status(200).send("<h1>Hello World</h1>");
 });
 
-app.get("/users", (req, res) => {
-    const users = [
-        {
-            name : "John Doe",
-            email : "john@doe.com",
-        },
-        {
-            name : "Jane Doe",
-            email : "jane@doe.com",
-        },
-    ];
+app.get("/users", async (req, res) => {
+    try{
+        const users = await UserModel.find({})
 
-    res.status(200).json(users)
+        res.status(200).json(users)
+
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
+})
+
+app.post("/users", async (req, res) => {  //criar usuário
+    try{
+        const user = await UserModel.create(req.body)
+
+        res.status(201).json(user)
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+
 })
 
 app.listen(port, () => console.log(`Rodando com Express na porta ${port}!`))
