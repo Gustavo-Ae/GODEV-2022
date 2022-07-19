1dc8fc342f3519d52961a26a5707a634b3e797f5
Gustavo-Ae <gustavoaedias05@hotmail.com>
2022-07-19 16:44:10 -0300

    Adição de uma tarefa e utilizei a biblioteca uuid para gerar números aleátorios.

--- Aulas_Gravadas/curso-react/src/App.jsx
+++ Aulas_Gravadas/curso-react/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 import Tasks from "./components/Tasks";
 import "./App.css"
@@ -21,11 +22,23 @@ const App = () => {
     },
   ]);
 
+  const handleTaskAddition = (taskTitle) => {
+      const newTasks = [...tasks, 
+          {
+            title: taskTitle,
+            id: uuidv4(),
+            completed: false,
+          },
+      ];
+      
+      setTasks(newTasks)      
+  }
+
   return(
     <> 
       <div className="container">
-        <AddTask />
-        <Tasks tasks = {tasks}/>
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} />
       </div>
     </>
   ) 
--- Aulas_Gravadas/curso-react/src/components/AddTask.jsx
+++ Aulas_Gravadas/curso-react/src/components/AddTask.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./AddTask.css"
 import Button from './Button';
 
-const AddTask = () => {
+const AddTask = ({handleTaskAddition}) => {
+
+    const [inputData, setInputData] = useState("")
+
+    const handleInputChange = (e) => { /* Pega as letras que o usuário digita */
+        setInputData(e.target.value)
+    }
+
+    const handleAddTaskClick = () => {
+        handleTaskAddition(inputData)
+        console.log("Palavra completa: "+inputData)
+        setInputData("")
+    }
+
     return (
         <div className='add-task-container'>
-            <input type="text" className='add-task-input'/>
+            <input onChange={handleInputChange} value={inputData} type="text" className='add-task-input'/>
             <div className="add-task-button-container">
-                <Button>Adicionar</Button>
+                <Button onClick={handleAddTaskClick}>Adicionar</Button>
             </div>
         </div>
      );
