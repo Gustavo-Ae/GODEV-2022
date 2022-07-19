b6ebd2b8f7372e913aca7f99f2c2380a3dc30db4
Gustavo-Ae <gustavoaedias05@hotmail.com>
2022-07-19 20:54:20 -0300

    resolvendo o bug de 'Quando clicar fica com a marca verde do lado da tarefa'

--- Aulas_Gravadas/curso-react/src/components/Task.jsx
+++ Aulas_Gravadas/curso-react/src/components/Task.jsx
@@ -4,9 +4,12 @@ import "./Task.css"
 
 const Task = ({task, handleTaskClick}) => {
     return (
-        <div className='task-container' onClick={() => handleTaskClick(task.id)} style={task.completed ? {borderLeft: "6px solid chartreuse"} : {} }>
-                {task.title}   
-        </div>
+            <div className='task-container' style={task.completed ? {borderLeft: "6px solid chartreuse"} : {} }>
+                
+                <div className='task-title' onClick={() => handleTaskClick(task.id)}>
+                    {task.title}
+                </div>
+            </div>    
     )
 }
  
